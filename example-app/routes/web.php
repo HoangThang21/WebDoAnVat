c8caa75eb12661bc0ee47d0d2d3c1ebada1f42b8
Thịnh Nhất Lê <68993699+thinhnhatle2002@users.noreply.github.com>
2024-02-23 12:16:51 +0700

    cc

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('Client.index');
 });
+Route::get('/contact', function () {
+    return view('Client.contact');
+});
