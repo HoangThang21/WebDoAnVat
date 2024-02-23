2fe1b420074ec6b63cd1f293deffcc2cb7b96b5f
Thịnh Nhất Lê <68993699+thinhnhatle2002@users.noreply.github.com>
2024-02-23 19:24:24 +0700

    check1

--- example-app/resources/views/layoutsClient/header.blade.php
+++ example-app/resources/views/layoutsClient/header.blade.php
@@ -7,40 +7,40 @@
 
 <!-- Navbar start -->
 <div class="container-fluid fixed-top">
-    <div class="container topbar bg-primary d-none d-lg-block">
+    <div class="container topbar  d-none d-lg-block" style="background-color: #FEF889;">
         <div class="d-flex justify-content-between">
             <div class="top-info ps-2">
-                <small class="me-3"><i class="fas fa-map-marker-alt me-2 text-secondary"></i> <a href="#" class="text-white">Hà Hoàng Hổ, Long Xuyên, An Giang</a></small>
-                <small class="me-3"><i class="fas fa-envelope me-2 text-secondary"></i><a href="#" class="text-white">nhom2@gmail.com</a></small>
+                <small class="me-3"><i class="fas fa-map-marker-alt me-2 text-secondary"></i> <a href="#" class="text-dark">Hà Hoàng Hổ, Long Xuyên, An Giang</a></small>
+                <small class="me-3"><i class="fas fa-envelope me-2 text-secondary"></i><a href="#" class="text-dark">nhom_2@gmail.com</a></small>
             </div>
             <div class="top-link pe-2">
-                <a href="#" class="text-white"><small class="text-white mx-2">Privacy Policy</small>/</a>
-                <a href="#" class="text-white"><small class="text-white mx-2">Terms of Use</small>/</a>
-                <a href="#" class="text-white"><small class="text-white ms-2">Sales and Refunds</small></a>
+                <a href="#" class="text-white"><small class="text-dark mx-2">Privacy Policy</small>/</a>
+                <a href="#" class="text-white"><small class="text-dark mx-2">Terms of Use</small>/</a>
+                <a href="#" class="text-white"><small class="text-dark ms-2">Sales and Refunds</small></a>
             </div>
         </div>
     </div>
     <div class="container px-0">
         <nav class="navbar navbar-light bg-white navbar-expand-xl">
-            <a href="/" class="navbar-brand"><h1 class="text-primary display-6">Food</h1></a>
+            <a href="/" class="navbar-brand"><h1 class="text-warning display-6">Food</h1></a>
             <button class="navbar-toggler py-2 px-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                 <span class="fa fa-bars text-primary"></span>
             </button>
             <div class="collapse navbar-collapse bg-white" id="navbarCollapse">
                 <div class="navbar-nav mx-auto">
-                    <a href="/" class="nav-item nav-link">Home</a>
-                    <a href="shop" class="nav-item nav-link">Shop</a>
-                    <a href="shop-detail" class="nav-item nav-link">Shop Detail</a>
+                    <a href="/" class="nav-item nav-link">Trang chủ</a>
+                    <a href="shop" class="nav-item nav-link">Cửa hàng</a>
+                    <a href="shop-detail" class="nav-item nav-link">Thông tin cửa hàng</a>
                     <div class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle active" data-bs-toggle="dropdown">Pages</a>
+                        <a href="#" class="nav-link dropdown-toggle active" data-bs-toggle="dropdown">Xem thêm</a>
                         <div class="dropdown-menu m-0 bg-secondary rounded-0">
                            
-                            <a href="checkout" class="dropdown-item">Checkout</a>
-                            <a href="testimonial" class="dropdown-item">Testimonial</a>
-                            <a href="404.html" class="dropdown-item">404 Page</a>
+                            <a href="checkout" class="dropdown-item">Thanh toán</a>
+                            <a href="testimonial" class="dropdown-item">Feedback</a>
+                            <!-- <a href="404.html" class="dropdown-item">404 Page</a> -->
                         </div>
                     </div>
-                    <a href="contact" class="nav-item nav-link">Contact</a>
+                    <a href="contact" class="nav-item nav-link">Liên Hệ</a>
                 </div>
                 <div class="d-flex m-3 me-0">
                     <button class="btn-search btn border border-secondary btn-md-square rounded-circle bg-white me-4" data-bs-toggle="modal" data-bs-target="#searchModal"><i class="fas fa-search text-primary"></i></button>
